736323016ba3f82bbbd2342f8817b7b473699581
ebubar <ebubar@gmail.com>
2019-09-11 16:16:53 -0400

    Create addtest_soln.py

---
+++ addtest_soln.py
@@ -0,0 +1,7 @@
+"""
+Did you get stuck?  Can't figure out the answer?
+You really shouldn't look at answers...you cheeky devil.
+In case you are stuck, though, here is the solution...
+"""
+def add(a,b):
+    return(a+b)
